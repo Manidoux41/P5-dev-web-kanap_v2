a27eb73748af6baccfc07c2e9118cfe88ccc7d1e
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-11-08 21:54:13 +0100

    add loop for kanap show

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,19 @@
-console.log('Ca fonctionne');
 
 let url = 'http://localhost:3000/api/products'
 
 fetch(url)
-  .then((response) => {
-    response.json()
-    .then((data) => {
-        console.log(data);
-    })    
-})
-  .catch(err => console.log('Erreur: ' + err)); 
+  .then((response) => response.json())
+  .then((datas) => {
+    console.log(datas);
+    let items = document.getElementById('items');
+    for(let data of datas) {
+      items.innerHTML += `<a href="./product.html?id=${data._id}">
+      <article>
+        <img src=${data.imageUrl} alt="${data.altTxt}">
+        <h3 class="productName">${data.name}</h3>
+        <p class="productDescription">${data.description}</p>
+      </article>
+    </a>`}
+  })
+  .catch((err) => console.log('serveur indisponible:' + err))
+
