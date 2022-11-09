c67ff4ee2f940a527d5ab5dee5c5ee6af680c90b
superflyman90 <superflyman90@gmail.com>
2022-11-09 11:38:15 +0100

    suppress innerHTML

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,20 @@
-
 let url = 'http://localhost:3000/api/products'
 
 fetch(url)
   .then((response) => response.json())
   .then((products) => {
     console.log(products);
-    let items = document.getElementById('items');
-    for(let data of products) {
-      items.innerHTML += `<a href="./product.html?id=${data._id}">
-      <article>
-        <img src=${data.imageUrl} alt="${data.altTxt}">
-        <h3 class="productName">${data.name}</h3>
-        <p class="productDescription">${data.description}</p>
-      </article>
-    </a>`}
-  })
-  .catch((err) => console.log('server unavailable:' + err))
 
+    for(let data of products) {
+        let display = ''
+        display += `<a href="./product.html?id=${data._id}">
+                        <article>
+                            <img src=${data.imageUrl} alt="${data.altTxt}">
+                            <h3 class="productName">${data.name}</h3>
+                            <p class="productDescription">${data.description}</p>
+                        </article>
+                    </a>`
+        document.getElementById('items').insertAdjacentHTML("beforeend", display)  
+    }
+})
+  .catch((err) => console.log('server unavailable:' + err))
